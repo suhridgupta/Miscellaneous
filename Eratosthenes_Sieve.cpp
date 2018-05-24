a9edf08392b50458fb76a0b0628edfbe04268656
suhridgupta <suhridgupta@gmail.com>
2018-05-24 10:07:00 +0530

    Finds prime numbers between 1 to N

---
+++ Eratosthenes_Sieve.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<bool>prime(n+1,true);
+    for(int i=2;i<sqrt(n+1);i++){
+        for(int j=0;j<=n;j=j+i)
+            prime[j] = false;
+    }
+    for(int i=0;i<=n;i++)
+        if(prime[i])    
+            cout<<i<<' ';
+    cout<<endl;
+    return 0;
+}
