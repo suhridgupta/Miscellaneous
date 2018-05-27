1fefe1d57e23cac5097b0156078fa85d07a3faa2
suhridgupta <suhridgupta@gmail.com>
2018-05-27 17:00:13 +0530

    Fixed inner loop

--- Eratosthenes_Sieve.cpp
+++ Eratosthenes_Sieve.cpp
@@ -9,7 +9,7 @@ int main(){
     cin>>n;
     vector<bool>prime(n+1,true);
     for(int i=2;i<sqrt(n+1);i++){
-        for(int j=0;j<=n;j=j+i)
+        for(int j=i*i;j<=n;j=j+i)
             prime[j] = false;
     }
     for(int i=0;i<=n;i++)
